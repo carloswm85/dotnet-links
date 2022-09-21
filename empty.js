2be9241594bd697faa43ca988176cfde0d65ce42
Carlos W. Mercado <carloswashingtonmercado@gmail.com>
2022-09-21 14:16:08 -0300

    simple update

---
+++ empty.js
@@ -0,0 +1,30 @@
+/*
+Escribe una función que verifique si en un string los paréntesis (), corchetes [] y llaves {} se encuentran balanceados y cierran de manera correcta.
+Por ejemplo:
+	"(2+3)*{2}" es un string balanceado y la función debe retornar true
+	y por ejemplo "(2]" false.
+Valores de prueba:
+"asdasd" -> true
+"()" -> true 
+"{{{}}" -> false
+"({[)}]" -> false
+"[](){}" -> true
+
+NOTA: Puede desarrollarlo en cualquier lenguaje de programación o pseudocodigo.
+*/
+
+a = "asdasd" // true
+b = "()" // true
+c = "{{{}}" // false
+d = "({[)}]" // false
+e = "[](){}" // true
+
+function test(text) {
+	var arr = split(text)
+	var conditon = split("[](){}")
+	array2 = arr.filter(char => char == condition != ,arr)
+	array2.paridad()
+}
+
+test(a)
+
